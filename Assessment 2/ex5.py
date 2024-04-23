4abd524c73b5b9138b46f2b8188e5b268f115570
ashithaangel <124872085+ashithaangel@users.noreply.github.com>
2024-04-23 19:39:34 +0530

    Add files via upload

---
+++ Assessment 2/ex5.py
@@ -0,0 +1,10 @@
+import json
+with open('ex5.json', 'r') as file:
+    ex5 = json.load(file)
+for item in ex5:
+    if item['name'] == 'Old Fashioned':
+        item['batters']['batter'].append({"id": "1003", "type": "Tea"})
+        break
+with open('ex5.json', 'w') as file:
+    json.dump(ex5, file, indent=4)
+print(ex5)
